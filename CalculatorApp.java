b9f25c8fb8037d25db0a71051a649b71ddfca655
rkmr17 <137607169+rkmr17@users.noreply.github.com>
2024-09-19 21:20:41 +0900

    インデント整理

--- CalculatorApp.java
+++ CalculatorApp.java
@@ -1,22 +1,22 @@
-import  java.util.Scanner;
+import java.util.Scanner;
 
 public class CalculatorApp {
-  //加算メソッド
+  // 加算メソッド
   public static double add(double a, double b) {
     return a + b;
   }
 
-  //減算メソッド
+  // 減算メソッド
   public static double subtract(double a, double b) {
     return a - b;
   }
 
-  //乗算メソッド
+  // 乗算メソッド
   public static double multiply(double a, double b) {
     return a * b;
   }
 
-  //除算メソッド
+  // 除算メソッド
   public static double divide(double a, double b) {
     if (b == 0) {
       System.out.println("0では割り切れません");
@@ -29,7 +29,7 @@ public class CalculatorApp {
     Scanner scanner = new Scanner(System.in);
     System.out.println("test");
 
-    //ユーザー入力処理
+    // ユーザー入力処理
     System.out.println("一つ目の数字を入力してください");
     double num1 = scanner.nextDouble();
 
@@ -39,11 +39,13 @@ public class CalculatorApp {
     System.out.println("二つ目の数字を入力してください");
     double num2 = scanner.nextDouble();
 
-    /*計算メソッド動作テスト
-    System.out.println("加算：" + add     (num1, num2));
-    System.out.println("減算：" + subtract(num1, num2));
-    System.out.println("乗算：" + multiply(num1, num2));
-    System.out.println("除算：" + divide  (num1, num2));*/
+    /*
+     * 計算メソッド動作テスト
+     * System.out.println("加算：" + add (num1, num2));
+     * System.out.println("減算：" + subtract(num1, num2));
+     * System.out.println("乗算：" + multiply(num1, num2));
+     * System.out.println("除算：" + divide (num1, num2));
+     */
   }
 
 }
