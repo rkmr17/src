5e71825413b3ab177cac69ebd23af8f99f06f73d
rkmr17 <137607169+rkmr17@users.noreply.github.com>
2024-09-19 20:40:35 +0900

    addメソッドを定義

--- CalculatorApp.java
+++ CalculatorApp.java
@@ -1,6 +1,11 @@
 public class CalculatorApp {
+  //加算メソッド
+  public static double add(double a, double b) {
+    return a + b;
+  }
+
   public static void main(String[] args) {
-    System.out.printin("test");
+    System.out.println("test");
   }
 
 }
