776efc83a9c9f817a21aee3cf1033bda2ce4b0a5
rkmr17 <137607169+rkmr17@users.noreply.github.com>
2024-09-19 20:59:13 +0900

    計算メソッド動作確認

--- CalculatorApp.java
+++ CalculatorApp.java
@@ -25,6 +25,18 @@ public class CalculatorApp {
 
   public static void main(String[] args) {
     System.out.println("test");
+
+    //計算メソッド動作テスト
+    double num1 = 10;
+    double num2 = 5;
+
+    System.out.println("加算：" + add     (num1, num2));
+    System.out.println("減算：" + subtract(num1, num2));
+    System.out.println("乗算：" + multiply(num1, num2));
+    System.out.println("除算：" + divide  (num1, num2));
+    
+    num2 = 0;
+    System.out.println("除算：" + divide  (num1, num2));
   }
 
 }
