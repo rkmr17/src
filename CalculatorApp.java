d2d4d7e5039cab1957f5c323c13bb88c951e8b0b
rkmr17 <137607169+rkmr17@users.noreply.github.com>
2024-09-19 20:53:03 +0900

    除算メソッドを定義

--- CalculatorApp.java
+++ CalculatorApp.java
@@ -13,7 +13,16 @@ public class CalculatorApp {
   public static double multiply(double a, double b) {
     return a * b;
   }
-  
+
+  //除算メソッド
+  public static double divide(double a, double b) {
+    if (b == 0) {
+      System.out.println("0では割り切れません");
+      return 0;
+    }
+    return a / b;
+  }
+
   public static void main(String[] args) {
     System.out.println("test");
   }
